83ec0152edf3e3675965ff433cef438f73a6f268
7rahib <rahibrashid002@gmail.com>
2022-09-28 16:58:48 +0600

    copy feature added when button clicked

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello');
+function copyShortUrl() {
+    let copyText = document.getElementById('short-url').innerHTML;
+
+    document.addEventListener('copy', function (e) {
+        e.clipboardData.setData('text/plain', copyText);
+        e.preventDefault();
+    }, true);
+    alert('copied text: ' + copyText);
+}
+
+function copyLongUrl() {
+    let copyText = document.getElementById('long-url').innerHTML;
+
+    document.addEventListener('copy', function (e) {
+        e.clipboardData.setData('text/plain', copyText);
+        e.preventDefault();
+    }, true);
+    alert('copied text: ' + copyText);
+}
